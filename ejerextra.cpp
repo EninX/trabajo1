3324efbf0793ecd9e20a0fdee7b6ec25f53554d7
vmanueltole1 <37409075+vmanueltole1@users.noreply.github.com>
2018-04-03 03:00:41 -0500

    Update ejerextra.cpp

--- ejerextra.cpp
+++ ejerextra.cpp
@@ -1,20 +1,17 @@
 #include <iostream>
 using namespace std;
-int factorial(int);
+
 int main()
 {
       int n;
+      int facr=1;
       cout<<"Ingresar numero  :";
       cin>>n;
-      cout<<"factorial del numero es :"<<factorial(n);
+      for(int i=1;i<=n;i++)
+      {
+            fact=i*fact;     
+      }
+      cout<<"el factorial de n es :"<<fact;
       return 0;
+}
 
-}
-int factorial(int n)
-{
-    int fac=1;
-    if(n==0)
-        return 1;
-    else
-        return n*factorial(n-1);
-}
